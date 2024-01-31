458535c974395ca867a133b5c959b9d6708c28ec
mateuszmalektdp <mm301915@student.polsl.pl>
2024-01-31 10:11:18 +0100

    Added ProcessPixel
    Added ProcessPixel

--- AssemblyProject/JAAsm/JAAsm.asm
+++ AssemblyProject/JAAsm/JAAsm.asm
@@ -1,9 +1,9 @@
 .data
 
     waga1 dd 1.0, 1.0, 1.0, 1.0
-    waga2 dd 2.0, 2.0, 2.0, 2.0 
-    waga4 dd 4.0, 4.0, 4.0, 4.0
-    wagaDiv dd 16.0, 16.0, 16.0, 16.0
+    waga2 dd 1.0, 1.0, 1.0, 1.0
+    waga4 dd 1.0, 1.0, 1.0, 1.0
+    wagaDiv dd 9.0, 9.0, 9.0, 9.0
     minus dd -1.0, -1.0, -1.0, -1.0
     laplace dd 5.0, 5.0, 5.0, 5.0
 
@@ -32,262 +32,40 @@ startLoop:
 
     mov eax, dword ptr [RCX - 36]    ; Lewy Górny s¹siad [X - -]
     movups xmm9, dword ptr [waga1]   ; Za³aduj wagê 1.0  [- X -]
-    ;call processPixel               ;                   [- - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
-
-
-
+    call processPixel                ;                   [- - -]
 
     mov eax, dword ptr [RCX - 32]    ; Górny s¹siad      [- X -]
     movups xmm9, dword ptr [waga2]   ; Za³aduj wagê 2.0  [- - -]
-    ;call processPixel               ;                   [- - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
-
-
+    call processPixel                ;                   [- - -]
 
     mov eax, dword ptr [RCX - 28]    ; Prawy górny s¹siad[- - X]
     movups xmm9, dword ptr [waga1]   ; Za³aduj wagê 1.0  [- - -]
-    ;call processPixel               ;                   [- - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- - -]
 
     mov eax, dword ptr [RCX - 4]     ; Lewy s¹siad       [- - -]
     movups xmm9, dword ptr [waga2]   ; Za³aduj wagê 2.0  [X - -]
-    ;call processPixel               ;                   [- - -]
-
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- - -]
 
 
     mov eax, dword ptr [RCX]         ; Bie¿¹cy piksel    [- - -]
     movups xmm9, dword ptr [waga4]   ; Za³aduj wagê 4.0  [- X -]
-    ;call processPixel               ;                   [- - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- - -]
 
     mov eax, dword ptr [RCX + 4]     ; Prawy s¹siad      [- - -]
     movups xmm9, dword ptr [waga2]   ; Za³aduj wagê 2.0  [- - X]
-    ;call processPixel               ;                   [- - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- - -]
 
     mov eax, dword ptr [RCX + 28]    ; Lewy dolny s¹siad [- - -]
     movups xmm9, dword ptr [waga1]   ; Za³aduj wagê 1.0  [- - -]
-    ;call processPixel               ;                   [X - -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [X - -]
 
     mov eax, dword ptr [RCX + 32]    ; Dolny s¹siad      [- - -]
     movups xmm9, dword ptr [waga2]   ; Za³aduj wagê 2.0  [- - -]
-    ;call processPixel               ;                   [- X -]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- X -]
 
     mov eax, dword ptr [RCX + 36]    ; Prawy dolny s¹siad[- - -]
     movups xmm9, dword ptr [waga1]   ; Za³aduj wagê 1.0  [- - -]
-    ;call processPixel               ;                   [- - X]
-
-                              ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
-    mov R9d, eax              ; Przekazanie piksela do rejestru R9
-    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
-    shr R9d, 16               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
-
-    mov R10d, eax             ; Przekazanie piksela do rejestru R10
-    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
-    shr R10d, 8               ; Przesuniecie kanalu na LSB
-    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
-
-    mov R11d, eax             ; Przekazanie piksela do rejestru R11
-    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
-    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
-
-                             ; £¹czenie kana³ów R, G, B w xmm0
-    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
-    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
-    unpcklps xmm3, xmm2      ; £¹czenie R i G
-    movlhps xmm3, xmm1       ; Dodanie B do xmm0
-
-    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
-    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    call processPixel                ;                   [- - X]
 
 
     divps xmm0, wagaDiv 
@@ -325,8 +103,31 @@ continueLoop:
     add rax, 10
     ret
 
+processPixel:
+                          ; Ekstrakcja kana³ów i konwersja na format zmiennoprzecinkowy
+    mov R9d, eax              ; Przekazanie piksela do rejestru R9
+    and R9d, redMask          ; Ekstrakcja kana³u czerwonego
+    shr R9d, 16               ; Przesuniecie kanalu na LSB
+    cvtsi2ss xmm1, R9d        ; Konwersja czerwonego na zmiennoprzecinkowe i za³adowanie do xmm0
 
+    mov R10d, eax             ; Przekazanie piksela do rejestru R10
+    and R10d, greenMask       ; Ekstrakcja kana³u zielonego
+    shr R10d, 8               ; Przesuniecie kanalu na LSB
+    cvtsi2ss xmm2, R10d       ; Konwersja zielonego na zmiennoprzecinkowe i za³adowanie do xmm1
 
+    mov R11d, eax             ; Przekazanie piksela do rejestru R11
+    and R11d, blueMask        ; Ekstrakcja kana³u niebieskiego
+    cvtsi2ss xmm3, R11d       ; Konwersja niebieskiego na zmiennoprzecinkowe i za³adowanie do xmm2
+
+                             ; £¹czenie kana³ów R, G, B w xmm0
+    shufps xmm3, xmm3, 0     ; Duplikacja wartoœci R
+    shufps xmm2, xmm2, 0     ; Duplikacja wartoœci G
+    unpcklps xmm3, xmm2      ; £¹czenie R i G
+    movlhps xmm3, xmm1       ; Dodanie B do xmm0
+
+    mulps xmm3, xmm9         ; Przemno¿enie piksela przez wage
+    addps xmm0, xmm3         ; Dodanie do xmm0 (zainicjalizowane)
+    ret
 GaussianBlur endp
 
 LaplacianFilter proc
